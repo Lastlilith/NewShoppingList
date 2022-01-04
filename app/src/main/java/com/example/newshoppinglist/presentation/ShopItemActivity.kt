8966dbd96c23ec004cd27f64d0ff73b768db93f2
Vladimir Vorozhtcov <lastlilith.2014@gmail.com>
2022-01-04 20:35:22 +0900

    Created View Model

--- app/src/main/java/com/example/newshoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/newshoppinglist/presentation/ShopItemActivity.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.newshoppinglist.R
 
 class ShopItemActivity : AppCompatActivity() {
+    private lateinit var viewModel: ShopItemViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+
     }
 }
