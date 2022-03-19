38494ebba03e246050736548c2ec3c242dcc2464
Vladimir Vorozhtcov <lastlilith.2014@gmail.com>
2022-03-19 13:44:10 +0900

    Implemented very basic ContentProvider

--- app/src/main/java/com/example/newshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newshoppinglist/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.newshoppinglist.presentation
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -35,6 +36,15 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedList
                 launchFragment(ShopItemFragment.newInstanceAddItem())
             }
         }
+
+        contentResolver.query(
+            Uri.parse("content://com.example.newshoppinglist/shop_items/3"),
+            null,
+            null,
+            null,
+            null,
+            null
+        )
     }
 
     override fun onEditingFinished() {
